b3c714b4949bc0e8d11cb95a0ff7d440b89be14d
unknown <jelenu@fp.insjoaquimmir.cat>
2023-05-04 12:47:18 +0200

    Api artistas y token terminado

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,6 +1,8 @@
 from rest_framework import routers
-from .api import ArtistViewSet, UserViewSet, CustomAuthToken
+from .api import ArtistViewSet, UserViewSet
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 
 router = routers.DefaultRouter()
@@ -10,5 +12,6 @@ router.register('api/users', UserViewSet, 'users')
 
 
 urlpatterns = [
-    path('api/token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
