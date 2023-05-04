3cd918974343a618f9dee5e7b71341b8e0bd55f8
Oriol <ormoin@fp.insjoaquimmir.cat>
2023-05-04 17:17:40 +0200

    Api

--- BDP/settings.py
+++ BDP/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'administracion',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
 ]
 
@@ -52,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'BDP.urls'
@@ -140,3 +143,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
