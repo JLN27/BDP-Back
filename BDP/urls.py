bcebb1a4d8388f86dd25d0936630b87a976c08d5
unknown <jelenu@fp.insjoaquimmir.cat>
2023-05-18 14:36:16 +0200

    retoques

--- BDP/urls.py
+++ BDP/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', lambda request: redirect('admin/', permanent=True)),
+
+    path('admin/', admin.site.urls),
     path('api/', include('administracion.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
